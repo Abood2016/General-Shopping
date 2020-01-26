53118aaf0f24b3fa43261f5ca10d92ca47603f07
Abood2016 <abooody4693@gmail.com>
2020-01-26 13:27:02 +0300

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+    'title', 'description',
+    'unit','price',
+    'total','category_id'
+];
+
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    
+
+
+}
